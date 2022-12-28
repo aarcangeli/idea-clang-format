3470671728d3030923086436259211055ef82e96
Alessandro Arcangeli <alessandroarcangeli.rm@gmail.com>
2022-12-29 00:30:45 +0100

    Initial project setup

--- src/main/kotlin/com/github/aarcangeli/ideaclangformat/services/MyProjectService.kt
+++ src/main/kotlin/com/github/aarcangeli/ideaclangformat/services/MyProjectService.kt
@@ -1,15 +1,12 @@
 package com.github.aarcangeli.ideaclangformat.services
 
-import com.intellij.openapi.project.Project
 import com.github.aarcangeli.ideaclangformat.MyBundle
+import com.intellij.openapi.project.Project
 
 class MyProjectService(project: Project) {
 
     init {
         println(MyBundle.message("projectService", project.name))
-
-        System.getenv("CI")
-            ?: TODO("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
     }
 
     /**
