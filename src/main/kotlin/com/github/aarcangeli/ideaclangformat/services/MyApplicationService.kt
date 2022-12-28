f9580a5a70c4b16caa1dc960f269883295b2ff6b
GitHub Action <action@github.com>
2022-12-28 22:51:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/aarcangeli/ideaclangformat/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/aarcangeli/ideaclangformat/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.aarcangeli.ideaclangformat.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.aarcangeli.ideaclangformat.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/aarcangeli/ideaclangformat/services/MyProjectService.kt
+++ src/main/kotlin/com/github/aarcangeli/ideaclangformat/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.aarcangeli.ideaclangformat.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.aarcangeli.ideaclangformat.MyBundle
 
 class MyProjectService(project: Project) {
 
